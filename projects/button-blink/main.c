dc4fba9135fece62a70b576c00fe376c5dbb652c
Micah Perry <micahjp5158@gmail.com>
2023-11-20 18:54:00 -0500

    button-blink: WIP: Blink green LED on btn press
    Triggers an interrupt on user button rising edge which toggles the green
    LED.
    
    Known issues / planned improvements:
    - Enabling GPIOA clock disables SWD / debugging capability.
    - NVIC IQR register address is hard-coded, need to add a header and find
      the definition for this register.
    - Button press should cycle through LEDs instead of only toggling green.
    - Before waiting for interrupts, enable low power mode.

--- projects/button-blink/main.c
+++ projects/button-blink/main.c
@@ -14,25 +14,40 @@
 int main(void)
 {
     // Enable GPIOA and GPIOD clocks
-    SET_BITS(RCC_AHB1ENR, 0);
-    SET_BITS(RCC_AHB1ENR, (BIT0 | BIT3));
+    // SET_BITS(RCC_AHB1ENR, (BIT0 | BIT3));
+    SET_BITS(RCC_AHB1ENR, BIT3);
 
-    // Set user push button to input direction
+    // Enable SYSCFG clock
+    SET_BITS(RCC_APB2ENR, BIT14);
+
+    // Set PA0 (user push button) to input direction
     CLEAR_BITS(GPIOA_MODER, (BIT0 | BIT1));
 
     // Set LED GPIOs to output direction
     SET_BITS(GPIOD_MODER, (BIT24 | BIT26 | BIT28 | BIT30));
 
-    while(1) {
-        for (int i = 0; i < 4; i++) {
-            // Toggle LED
-            CLEAR_REG(GPIOD_ODR);
-            SET_BITS(GPIOD_ODR, (BIT12 << i));
+    // Configure PA0 (user push button) to trigger EXTI0 interrupt
+    CLEAR_BITS(SYSCFG_EXTICR1, (BIT0 | BIT1 | BIT2 | BIT3));
 
-            // Delay
-            for (volatile unsigned int i = 0; i < DELAY; i++);
-        }
-    }
+    // Unmask EXTI0
+    SET_BITS(EXTI_IMR, BIT0);
 
+    // Trigger EXTI0 on rising edge
+    SET_BITS(EXTI_RTSR, BIT0);
+
+    // NVIC IRQ enable
+    SET_BITS(0xE000E100, BIT6);
+
+    // Wait for interrupts
+    while(1);
     return 0;
+}
+
+void EXTI0_IRQHandler(void) {
+    // Toggle LED
+    //TOGGLE_BITS(GPIOD_ODR, BIT12);
+    TOGGLE_BITS(GPIOD_ODR, BIT12);
+
+    // Clear pending interrupt
+    SET_BITS(EXTI_PR, BIT0);
 }

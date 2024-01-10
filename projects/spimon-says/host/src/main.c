38f240b1aa1d15761781997c5281ec6b4d6b2330
Micah Perry <micahjp5158@gmail.com>
2024-01-09 20:53:39 -0500

    spimon-says: Add basic LED function for host
    Adds the basic framework for selecting an LED and blinking it. Later
    additions will use the RNG peripheral to randomly select the LED and
    send that selection to a client STM32 via SPI.

--- projects/spimon-says/host/src/main.c
+++ projects/spimon-says/host/src/main.c
@@ -8,9 +8,54 @@
  * instructing the client to illuminate the same LED.
  */
 
+#include "stdint.h"
+
 #include "stm32f407vg.h"
+
+#define LED_GRN     BIT12
+#define LED_YLW     BIT13
+#define LED_RED     BIT14
+#define LED_BLU     BIT15
+
+const uint32_t led_lookup[] = {LED_GRN, LED_YLW, LED_RED, LED_BLU};
+
+// Arbitrary delay
+#define DELAY   400000
 
 int main(void)
 {
+    // Enable GPIOD clock
+    ACCESS(RCC_AHB1ENR) |= RCC_AHB1ENR_GPIODEN;
 
+    // Set LED GPIOs to output direction
+    ACCESS(GPIOD_MODER) |= BIT24 | BIT26 | BIT28 | BIT30;
+
+    // TODO: Configure RNG
+
+    // TODO: Configure SPI
+
+    while(1) {
+        // Get a new random number 0-3
+        // TODO: Use RNG peripheral to generate a random value
+        uint8_t led_idx = 0;
+
+        // Blink selected LED three times (6 toggles)
+        for (uint8_t i = 0; i < 6; i++) {
+            ACCESS(GPIOD_ODR) ^= led_lookup[led_idx];
+
+            // Delay
+            for (volatile unsigned int j = 0; j < DELAY; j++);
+        }
+
+        // Hold the LED on for a few cycles
+        ACCESS(GPIOD_ODR) |= led_lookup[led_idx];
+        for (volatile unsigned int j = 0; j < (3 * DELAY); j++);
+
+        // Send the selected LED value to the client STM32 via SPI
+        // TODO: Send data over SPI
+
+        // Hold LEDs off for a few cycles
+        ACCESS(GPIOD_ODR) &= ~(led_lookup[led_idx]);
+        for (volatile unsigned int j = 0; j < (3 * DELAY); j++);
+    }
 }
